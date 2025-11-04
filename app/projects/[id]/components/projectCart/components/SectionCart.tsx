94bcfe8a624fad7dc74a3f53d2bc86a113b4c132
Jakub Roman <jakub.m.roman@gmail.com>
2025-11-04 08:55:04 +0100

    Vytvorenie UI  kostry pre zberanie dat z clock time pre presne definovanie stop time casov a datumu

--- app/projects/[id]/components/projectCart/components/SectionCart.tsx
+++ app/projects/[id]/components/projectCart/components/SectionCart.tsx
@@ -104,7 +104,7 @@ const SectionCart = ({ ...props }: SectionCart) => {
           const totalSeconds = h * 3600 + m * 60 + s;
 
           const currTime = new Date();
-          const offset = new Date(currTime.getTime() + totalSeconds * 1010);
+          const offset = new Date(currTime.getTime() + totalSeconds * 1005);
           setDataTime(offset);
         }
       }
@@ -144,37 +144,69 @@ const SectionCart = ({ ...props }: SectionCart) => {
     setIsModalOpen(false);
   };
 
+  const [sections, setSections] = useState([
+    {
+      id: 1,
+      startTime: "19:00",
+      stopTime: "20:05",
+      clockTime: "1:5:0",
+      date: "4.11.2025",
+    },
+  ]);
+
+  const isAnySections = sections.length > 0 ? true : false;
+
   return (
     <li
       key={props.sectionId}
-      className="border w-full h-18 rounded-2xl flex items-center justify-between"
+      className="border w-full rounded-2xl flex flex-col flex-between"
     >
-      <div className="w-4/12 flex items-center justify-start text-2xl pl-16">
-        <h1>{props.title}</h1>
+      <div className="w-full flex items-center justify-between py-4">
+        <div className="w-4/12 flex items-center justify-start text-2xl pl-6">
+          <h1>{props.title}</h1>
+        </div>
+        <div className="w-4/12 flex items-center justify-center text-2xl">
+          <span>{newTime}</span>
+        </div>
+        <div className="w-4/12 flex items-center justify-center gap-10">
+          <button
+            onClick={toggleTimer}
+            className="border px-3 py-1 rounded-2xl cursor-pointer"
+          >
+            {btnTittle}
+          </button>
+          <button
+            onClick={() => setIsModalOpen(true)}
+            className="border px-3 py-1 rounded-2xl cursor-pointer"
+          >
+            Delete
+          </button>
+        </div>
+        <DeleteModal
+          isModalOpen={isModalOpen}
+          setIsModalOpen={setIsModalOpen}
+          title={props.title}
+          btnFunction={() => deleteSection()}
+        />
       </div>
-      <div className="w-4/12 flex items-center justify-center text-2xl">
-        <span>{newTime}</span>
-      </div>
-      <div className="w-4/12 flex items-center justify-center gap-10">
-        <button
-          onClick={toggleTimer}
-          className="border px-3 py-1 rounded-2xl cursor-pointer"
-        >
-          {btnTittle}
-        </button>
-        <button
-          onClick={() => setIsModalOpen(true)}
-          className="border px-3 py-1 rounded-2xl cursor-pointer"
-        >
-          Delete
-        </button>
-      </div>
-      <DeleteModal
-        isModalOpen={isModalOpen}
-        setIsModalOpen={setIsModalOpen}
-        title={props.title}
-        btnFunction={() => deleteSection()}
-      />
+      <ul
+        className={`${
+          isAnySections ? "flex-1" : "hidden"
+        } w-full pt-2 pb-3 px-6 flex items-center gap-4 overflow-x-auto overflow-y-hidden`}
+      >
+        {sections.map((s, index) => (
+          <li
+            key={s.id}
+            className="border w-[150px] h-[100px] rounded-xl shrink-0 flex flex-col justify-between items-start p-2"
+          >
+            <span className="font-semibold">
+              h: {s.startTime} - {s.stopTime}
+            </span>
+            <span className="font-semibold">t: {s.clockTime}</span>
+            <span className="font-semibold">d: {s.date}</span>
+          </li>
+        ))}
+      </ul>
     </li>
   );
 };
